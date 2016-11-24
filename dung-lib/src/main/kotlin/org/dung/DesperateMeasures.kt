cbf8ec81f657bce2dd3dfd515873b82eef257887
Andrew <andypaterson1@live.com>
2016-11-24 22:06:21 +0000

    scheduling

--- dung-lib/src/main/kotlin/org/dung/DesperateMeasures.kt
+++ dung-lib/src/main/kotlin/org/dung/DesperateMeasures.kt
@@ -1,15 +1,12 @@
 package org.dung
 
+import java.time.Duration
+
 fun main(args: Array<String>) {
     runMud({ world -> DesperateMeasures(world) })
 }
 
 class DesperateMeasures(world: MudWorld) : MudCore(world) {
-
-    fun castleGuard(id: Int, name: String) =
-            EntityImpl(id).modify()
-                    .set(nameTrait, name)
-                    .set(descriptionTrait, "A tall, strong guard from the castle barracks. He's well-trained and well-equipped")
 
     val location_start = createEntity("Bedroom", """
 Your bedroom, night-time. Light shines around the edges of a tiny door, set in the north wall, that you have never noticed before.
@@ -24,5 +21,9 @@ A long and winding tunnel, stretching from your bedroom to some unknown location
 A dismal castle, swept by rain from low, tattered clouds. Its walls are made from thick dark stone. Firelight flickers
 fitfully through arrow slits.
 """)
+    val castleGuard = createEntity("Bob", "A tall, strong guard from the castle barracks. He's well-trained and well-equipped") {
+        set(locationTrait, location_castle_entrance)
+                .repeatAtRandom(Duration.ofSeconds(2), Duration.ofSeconds(20), entityCommand({ entity, currentFrame, nextFrame -> println("${entity.get(nameTrait)} picks his nose")}))
+    }
     init { linkLocations(location_start_tunnel, location_castle_entrance, ::north, ::south) }
 }
--- dung-lib/src/main/kotlin/org/dung/mudkernel.kt
+++ dung-lib/src/main/kotlin/org/dung/mudkernel.kt
@@ -2,17 +2,18 @@ package org.dung
 
 import rx.Observable
 import rx.subjects.PublishSubject
+import java.time.Duration
 import java.time.LocalDateTime
-import java.util.HashMap
+import java.time.temporal.TemporalAmount
+import java.util.*
 import java.util.concurrent.ArrayBlockingQueue
 import java.util.concurrent.atomic.AtomicInteger
 
 
-class TraitType<T>(val createDefault: ()->T) {
-
-}
+class TraitType<T>(val createDefault: ()->T)
 
 interface TraitBased {
+    val traits: Map<TraitType<*>, Any>
     fun <T> get(t: TraitType<T>): T?
 }
 
@@ -26,8 +27,17 @@ interface EntityBuilder : TraitBased {
     fun <T> set(t: TraitType<T>, value: T): EntityBuilder
     fun build(): Entity
 }
+fun <T> EntityBuilder.getOrCreate(t: TraitType<T>, create: ()->T = t.createDefault): T {
+    var trait = get(t)
+    if (trait != null) {
+        return trait
+    }
+    trait = create()
+    set(t, trait)
+    return trait
+}
 
-class EntityBuilderImpl(override val id: Int, private val traits: MutableMap<TraitType<*>, Any>) : EntityBuilder {
+class EntityBuilderImpl(override val id: Int, override val traits: MutableMap<TraitType<*>, Any>) : EntityBuilder {
     override fun <T> get(t: TraitType<T>): T? = traits[t]?.run { this as T }
 
     override fun <T> set(t: TraitType<T>, value: T): EntityBuilder = apply { traits[t] = value as Any }
@@ -36,7 +46,7 @@ class EntityBuilderImpl(override val id: Int, private val traits: MutableMap<Tra
 
 }
 
-class EntityImpl(override val id: Int, private val traits: Map<TraitType<*>, Any> = mapOf()) : Entity, EntityBuilder {
+class EntityImpl(override val id: Int, override val traits: Map<TraitType<*>, Any> = mapOf()) : Entity, EntityBuilder {
     override fun <T> set(t: TraitType<T>, value: T): EntityBuilder = EntityBuilderImpl(id, HashMap(traits)).set(t, value)
 
     override fun build(): Entity = this
@@ -80,7 +90,7 @@ class MudClientConsoleView(private val world: MudWorld) : MudClientView {
                 .set(nameTrait, "Dylan")
                 .set(locationTrait, 0)
                 .build()
-        world.addCommand { frame, frameBuilder ->
+        world.addCommand(command({ frame, frameBuilder ->
             frameBuilder.addEntity(player)
             show("Hello ${player.get(nameTrait)}")
             val playerLocationId = player.get(locationTrait)
@@ -90,18 +100,35 @@ class MudClientConsoleView(private val world: MudWorld) : MudClientView {
             else {
                 show(frame.entitiesById[playerLocationId]?.get(descriptionTrait)?:"You are at an unknown location")
             }
-        }
+        }))
     }
-
     fun run() {
         var quit = false
         do {
-            val line = readLine()
-            when (line) {
-                in arrayOf("q", "quit", "exit") -> quit = true
+            val line = readLine()?.split(" ")?:listOf()
+            if (line.isEmpty()) {
+                continue
+            }
+            when {
+                line[0] in arrayOf("q", "quit", "exit") -> quit = true
                 else -> {
-                    world.addCommand { f, fb ->
+                    world.addCommand(command({ f, fb ->
                         fb.updateEntity(playerId, fun EntityBuilder.(): EntityBuilder {
+                            fun processVerb(onMatch: (Entity)->Unit) {
+                                if (line.size != 2) {
+                                    return
+                                }
+                                val targetName = line[1].toUpperCase()
+                                val target = f.entities.firstOrNull { e -> e.get(nameTrait)?.toUpperCase()?.equals(targetName)?:false }
+                                if (target != null) {
+                                    onMatch(target)
+                                }
+                            }
+                            when (line[0]) {
+                                "inspect" -> processVerb { e->show(e.get(descriptionTrait)?:"") }
+                                "get" -> processVerb {
+                                }
+                            }
                             val currentLocation = get(locationTrait)?.run { f.entitiesById[this] }
                             if (currentLocation == null) {
                                 return this
@@ -110,10 +137,10 @@ class MudClientConsoleView(private val world: MudWorld) : MudClientView {
                             if (links == null) {
                                 return this
                             }
-                            val link = links.firstOrNull { it.commandBinding.contains(line) }
+                            val link = links.firstOrNull { it.commandBinding.contains(line[0]) }
                             return if (link == null) this else set(locationTrait, link.destination)
                         })
-                    }
+                    }))
                 }
             }
         } while (!quit);
@@ -130,12 +157,20 @@ class MudWorldView(val entityId: Int, world: MudWorld, view: MudClientView) {
                 val locationInLastFrame = entityInLastFrame.get(locationTrait) ?: -1
                 val locationInCurrentFrame = entityInCurrentFrame.get(locationTrait) ?: -1
                 if (locationInLastFrame != locationInCurrentFrame) {
-                    currentFrame.entitiesById[locationInCurrentFrame]?.apply {
-                        view.show(get(descriptionTrait)?:"")
+                    currentFrame.entitiesById[locationInCurrentFrame]?.let { location->
+                        view.show(location.get(descriptionTrait)?:"")
                         val entitiesInLocation = currentFrame.entities.filter { it.get(locationTrait) == locationInCurrentFrame }
                         if (!entitiesInLocation.isEmpty()) {
                             view.show("You can see:")
                             entitiesInLocation.forEach { view.show(it.get(nameTrait)?:"[Unnamed Thing ${it.id}]") }
+                        }
+                        val exits = location.get(locationLinkTrait)?:arrayOf()
+                        if (exits.size == 0) {
+                            view.show("There are no exits")
+                        }
+                        else {
+                            view.show("Exits are:")
+                            exits.forEach { exit -> view.show("${exit.name} (type ${exit.commandBinding.joinToString(", or ")})")}
                         }
 
                     }
@@ -149,27 +184,52 @@ class SceneGraph(val locations: Array<Entity>) {
     val locationsById = locations.associate { it.id to it }
 }
 
+interface TickableTrait {
+    fun update(entity: Entity, updateTimestamp: LocalDateTime): List<WorldCommand>
+}
+
+interface WorldCommand {
+    fun run(frame: WorldFrame, nextFrame: WorldFrameBuilder)
+}
+interface WorldEntityCommand {
+    fun run(entity: Entity, frame: WorldFrame, nextFrame: WorldFrameBuilder)
+}
+fun command(f: (WorldFrame, WorldFrameBuilder)->Unit): WorldCommand {
+    return object : WorldCommand {
+        override fun run(frame: WorldFrame, nextFrame: WorldFrameBuilder)= f(frame, nextFrame)
+    }
+}
+fun entityCommand(f: (Entity, WorldFrame, WorldFrameBuilder)->Unit): WorldEntityCommand {
+    return object : WorldEntityCommand {
+        override fun run(entity: Entity, frame: WorldFrame, nextFrame: WorldFrameBuilder)= f(entity, frame, nextFrame)
+    }
+}
+
 class MudWorld() {
     var lastFrame = WorldFrame(arrayOf())
     var currentFrame = lastFrame
     val entityIdGen = AtomicInteger()
-    val commandQueue = ArrayBlockingQueue<(WorldFrame, WorldFrameBuilder)->Unit>(1000)
+    val commandQueue = ArrayBlockingQueue<WorldCommand>(1000)
     private val newFrameSubject = PublishSubject.create<Pair<WorldFrame, WorldFrame>>()
 
     val newFrame: Observable<Pair<WorldFrame, WorldFrame>> get() = newFrameSubject
 
     fun nextEntityId() = entityIdGen.andIncrement
 
-    fun addCommand(cmd: (WorldFrame, WorldFrameBuilder)->Unit) {
+    fun addCommand(cmd: WorldCommand) {
         commandQueue.add(cmd)
     }
 
+
     fun process() {
-        val commands = mutableListOf<(WorldFrame, WorldFrameBuilder)->Unit>()
+        val commands = mutableListOf<WorldCommand>()
         commandQueue.drainTo(commands)
 
+        val frameTimestamp = LocalDateTime.now()
+        commands.addAll(currentFrame.entities.flatMap { e -> e.traits.values.filterIsInstance<TickableTrait>().flatMap { it.update(e, frameTimestamp) } })
+
         val currentFrameBuilder = WorldFrameBuilder(currentFrame)
-        commands.forEach { command -> command(currentFrame, currentFrameBuilder) }
+        commands.forEach { command -> command.run(currentFrame, currentFrameBuilder) }
 
         currentFrame = currentFrameBuilder.build()
         newFrameSubject.onNext(lastFrame to currentFrame )
@@ -194,24 +254,79 @@ fun west(id: Int) = LocationLink(id, "west", arrayOf("w", "west"))
 fun down(id: Int) = LocationLink(id, "down", arrayOf("d", "down"))
 fun up(id: Int) = LocationLink(id, "up", arrayOf("u", "up"))
 
+data class EntityScheduleEntry(val executeAt: LocalDateTime, val commandToRun: WorldEntityCommand, val nextEntries: ()->Array<EntityScheduleEntry>)
+data class EntityScheduler(val schedule: List<EntityScheduleEntry> = listOf()) : TickableTrait {
+    override fun update(entity: Entity, updateTimestamp: LocalDateTime): List<WorldCommand> {
+
+        val newSchedule = mutableListOf<EntityScheduleEntry>()
+        val commands = mutableListOf<WorldCommand>()
+        for (entry in schedule) {
+            if (entry.executeAt > updateTimestamp) {
+                newSchedule.add(entry)
+            }
+            else {
+                newSchedule.addAll(entry.nextEntries())
+                if (commands.isEmpty()) {
+                    commands.add(command { currentFrame, nextFrame ->
+                        nextFrame.updateEntity(entity.id, { set(schedulerTrait, EntityScheduler(newSchedule) )})
+                    })
+                }
+                commands.add(command({frame, nextFrame->entry.commandToRun.run(entity, frame, nextFrame)}))
+            }
+        }
+
+        return commands
+    }
+    fun addEntry(entry: EntityScheduleEntry) = copy(schedule = schedule + entry)
+}
+val schedulerTrait = TraitType({ EntityScheduler() })
+
+fun EntityBuilder.addToSchedule(entry: EntityScheduleEntry): EntityBuilder {
+    return set(schedulerTrait, getOrCreate(schedulerTrait).addEntry(entry))
+}
+fun EntityBuilder.addToSchedule(delay: TemporalAmount, command: WorldEntityCommand): EntityBuilder {
+    return addToSchedule(EntityScheduleEntry(LocalDateTime.now().plus(delay), command, { arrayOf() }))
+}
+fun repeatingSheduleEntry(nextAt: ()->LocalDateTime, command: WorldEntityCommand): EntityScheduleEntry {
+    return EntityScheduleEntry(nextAt(), command, { arrayOf(repeatingSheduleEntry(nextAt, command)) })
+}
+fun EntityBuilder.repeatAtRandom(minDelay: TemporalAmount, maxDelay: TemporalAmount, command: WorldEntityCommand): EntityBuilder {
+    return addToSchedule(repeatingSheduleEntry({ randomTimeFromNow(minDelay, maxDelay) }, command))
+}
+val rnd = Random(-1)
+fun randomTimeFromNow(minDelay: TemporalAmount, maxDelay: TemporalAmount):LocalDateTime {
+    return LocalDateTime.now() + minDelay // TODO
+}
+
+//  TODO Affordances
+//  TODO Triggers
+//  TODO Battles
+
+//data class Affordance(commandBinding: Array<String>, ())
+
+val healthTrait = TraitType( { 0 })
+val strengthTrait = TraitType( { 0 })
+val accuracyTrait = TraitType( { 0 })
+val magicTrait = TraitType( { 0 })
 
 //  TODO Entities in locations
 //  TODO Different descriptions based on different conditions (embedded rule based descriptions)
 
 interface Mud {
-    fun createCommandsForInitialState(): Array<(WorldFrame, WorldFrameBuilder)->Unit>
+    fun createCommandsForInitialState(): Array<WorldCommand>
 }
 
 open class MudCore(private val world: MudWorld) : Mud {
 
-    override fun createCommandsForInitialState(): Array<(WorldFrame, WorldFrameBuilder)->Unit> = entityMap.values.map { { frame: WorldFrame, frameBuilder: WorldFrameBuilder -> frameBuilder.addEntity(it.build()) } }.toTypedArray()
+    override fun createCommandsForInitialState(): Array<WorldCommand> = entityMap.values.map {command({ frame: WorldFrame, frameBuilder: WorldFrameBuilder -> frameBuilder.addEntity(it.build())}) }.toTypedArray()
 
     private val entityMap = mutableMapOf<Int, EntityBuilder>()
 
-    protected fun createEntity(name: String, description: String): Int {
-        val entity = EntityImpl(world.nextEntityId()).modify()
-            .set(nameTrait, name)
-            .set(descriptionTrait, description)
+    protected fun createEntity(name: String, description: String, buildMore: EntityBuilder.()->EntityBuilder = { this }): Int {
+        val entity = EntityBuilderImpl(world.nextEntityId(), mutableMapOf())
+                .set(nameTrait, name)
+                .set(descriptionTrait, description)
+                .buildMore()
         entityMap[entity.id] = entity
         return entity.id
     }
@@ -222,10 +337,9 @@ open class MudCore(private val world: MudWorld) : Mud {
     }
 
     fun linkLocations(a: Int, b: Int, aToB: (Int) -> LocationLink, bToA: (Int) -> LocationLink) {
-        updateEntity(a, { addLocationLink(aToB(a)) })
-        updateEntity(b, { addLocationLink(bToA(b)) })
+        updateEntity(a, { addLocationLink(aToB(b)) })
+        updateEntity(b, { addLocationLink(bToA(a)) })
     }
-
 }
 
 fun runMud(makeMud: (MudWorld)->Mud) {
